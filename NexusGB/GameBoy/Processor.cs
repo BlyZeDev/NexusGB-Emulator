92b73e8ebb6dc97d53788e01750d912a8f4aa002
BlyZeDev <leonschimmel15@gmail.com>
2025-03-06 21:25:20 +0100

    Finished CPU Execute opcodes

--- NexusGB/GameBoy/Processor.cs
+++ NexusGB/GameBoy/Processor.cs
@@ -1,5 +1,7 @@
 ﻿namespace NexusGB.GameBoy;
 
+using System.Collections.Immutable;
+
 public sealed class Processor
 {
     private const int JUMP_RELATIVE_TRUE = 12;
@@ -10,6 +12,52 @@ public sealed class Processor
     private const int RETURN_FALSE = 8;
     private const int CALL_TRUE = 24;
     private const int CALL_FALSE = 12;
+
+    private static readonly ImmutableArray<int> _cyclesValues =
+    [
+        4, 12,  8,  8,  4,  4,  8,  4, 20,  8,  8,  8,  4,  4,  8,  4,
+	    4, 12,  8,  8,  4,  4,  8,  4,  0,  8,  8,  8,  4,  4,  8,  4,
+        0, 12,  8,  8,  4,  4,  8,  4,  0,  8,  8,  8,  4,  4,  8,  4,
+        0, 12,  8,  8, 12, 12, 12,  4,  0,  8,  8,  8,  4,  4,  8,  4,
+
+        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+	    4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+        8,  8,  8,  8,  8,  8,  4,  8,  4,  4,  4,  4,  4,  4,  8,  4,
+
+        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+	    4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,
+
+        0,  12,  0,  0,  0, 16, 8, 16,  0, -4,  0,  0,  0,  0,  8, 16,
+	    0,  12,  0, 00,  0, 16, 8, 16,  0, -4,  0, 00,  0, 00,  8, 16,
+        12, 12,  8, 00, 00, 16, 8, 16, 16,  4, 16, 00, 00, 00,  8, 16,
+        12, 12,  8,  4, 00, 16, 8, 16, 12,  8, 16,  4, 00, 00,  8, 16
+    ];
+
+    private static readonly ImmutableArray<int> _cyclesFixedValues =
+    [
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        
+        8,  8,  8,  8,  8,  8, 12,  8,  8,  8,  8,  8,  8,  8, 12,  8,
+        8,  8,  8,  8,  8,  8, 12,  8,  8,  8,  8,  8,  8,  8, 12,  8,
+        8,  8,  8,  8,  8,  8, 12,  8,  8,  8,  8,  8,  8,  8, 12,  8,
+        8,  8,  8,  8,  8,  8, 12,  8,  8,  8,  8,  8,  8,  8, 12,  8,
+        
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8,
+        8,  8,  8,  8,  8,  8, 16,  8,  8,  8,  8,  8,  8,  8, 16,  8
+    ];
 
     private readonly MemoryManagement _mmu;
 
@@ -114,36 +162,24 @@ public sealed class Processor
         switch (opCode)
         {
             case 0x00: break;
-            case 0x01:
-                BC = _mmu.ReadWord(programCounter);
-                programCounter += 2;
-                break;
+            case 0x01: BC = _mmu.ReadWord(programCounter += 2); break;
             case 0x02: _mmu.WriteByte(BC, regA); break;
             case 0x03: BC++; break;
             case 0x04: Increment(ref regB); break;
             case 0x05: Decrement(ref regB); break;
-            case 0x06:
-                regB = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x06: regB = _mmu.ReadByte(programCounter++); break;
             case 0x07:
                 regF = 0;
                 FlagC = (regA & 0x80) != 0;
                 regA = (byte)((regA << 1) | (regA >> 7));
                 break;
-            case 0x08:
-                _mmu.WriteWord(_mmu.ReadWord(programCounter), stackPointer);
-                programCounter += 2;
-                break;
+            case 0x08: _mmu.WriteWord(_mmu.ReadWord(programCounter += 2), stackPointer); break;
             case 0x09: DoubleAdd(BC); break;
             case 0x0A: regA = _mmu.ReadByte(BC); break;
             case 0x0B: BC--; break;
             case 0x0C: Increment(ref regC); break;
             case 0x0D: Decrement(ref regC); break;
-            case 0x0E:
-                regC = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x0E: regC = _mmu.ReadByte(programCounter++); break;
             case 0x0F:
                 regF = 0;
                 FlagC = (regA & 0x01) != 0;
@@ -151,18 +187,12 @@ public sealed class Processor
                 break;
 
             case 0x10: Stop(); break;
-            case 0x11:
-                DE = _mmu.ReadWord(programCounter);
-                programCounter += 2;
-                break;
+            case 0x11: DE = _mmu.ReadWord(programCounter += 2); break;
             case 0x12: _mmu.WriteByte(DE, regA); break;
             case 0x13: DE++; break;
             case 0x14: Increment(ref regD); break;
             case 0x15: Decrement(ref regD); break;
-            case 0x16:
-                regD = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x16: regD = _mmu.ReadByte(programCounter++); break;
             case 0x17:
                 {
                     var prevC = FlagC ? 1 : 0;
@@ -177,10 +207,7 @@ public sealed class Processor
             case 0x1B: DE--; break;
             case 0x1C: Increment(ref regE); break;
             case 0x1D: Decrement(ref regE); break;
-            case 0x1E:
-                regE = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x1E: regE = _mmu.ReadByte(programCounter++); break;
             case 0x1F:
                 {
                     var prevC = FlagC ? 0x80 : 0x00;
@@ -191,18 +218,12 @@ public sealed class Processor
                 break;
 
             case 0x20: JumpRelative(!FlagZ); break;
-            case 0x21:
-                HL = _mmu.ReadWord(programCounter);
-                programCounter += 2;
-                break;
+            case 0x21: HL = _mmu.ReadWord(programCounter += 2); break;
             case 0x22: _mmu.WriteByte(HL++, regA); break;
             case 0x23: HL++; break;
             case 0x24: Increment(ref regH); break;
             case 0x25: Decrement(ref regH); break;
-            case 0x26:
-                regH = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x26: regH = _mmu.ReadByte(programCounter++); break;
             case 0x27:
                 if (FlagN)
                 {
@@ -225,10 +246,7 @@ public sealed class Processor
             case 0x2B: HL--; break;
             case 0x2C: Increment(ref regL); break;
             case 0x2D: Decrement(ref regL); break;
-            case 0x2E:
-                regL = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x2E: regL = _mmu.ReadByte(programCounter++); break;
             case 0x2F:
                 regA = (byte)~regA;
                 FlagN = true;
@@ -236,10 +254,7 @@ public sealed class Processor
                 break;
 
             case 0x30: JumpRelative(!FlagC); break;
-            case 0x31:
-                stackPointer = _mmu.ReadWord(programCounter);
-                programCounter += 2;
-                break;
+            case 0x31: stackPointer = _mmu.ReadWord(programCounter += 2); break;
             case 0x32: _mmu.WriteByte(HL--, regA); break;
             case 0x33: stackPointer++; break;
             case 0x34:
@@ -256,10 +271,7 @@ public sealed class Processor
                     _mmu.WriteByte(HL, read);
                 }
                 break;
-            case 0x36:
-                _mmu.WriteByte(HL, _mmu.ReadByte(programCounter));
-                programCounter++;
-                break;
+            case 0x36: _mmu.WriteByte(HL, _mmu.ReadByte(programCounter++)); break;
             case 0x37:
                 FlagC = true;
                 FlagN = false;
@@ -271,10 +283,7 @@ public sealed class Processor
             case 0x3B: stackPointer--; break;
             case 0x3C: Increment(ref regA); break;
             case 0x3D: Decrement(ref regA); break;
-            case 0x3E:
-                regA = _mmu.ReadByte(programCounter);
-                programCounter++;
-                break;
+            case 0x3E: regA = _mmu.ReadByte(programCounter++);break;
             case 0x3F:
                 FlagC = !FlagC;
                 FlagN = false;
@@ -316,12 +325,192 @@ public sealed class Processor
             case 0x58: regE = regB; break;
             case 0x59: regE = regC; break;
             case 0x5A: regE = regD; break;
+
             case 0x5B: break;
+
             case 0x5C: regE = regH; break;
             case 0x5D: regE = regL; break;
             case 0x5E: regE = _mmu.ReadByte(HL); break;
             case 0x5F: regE = regA; break;
+
+            case 0x60: regH = regB; break;
+            case 0x61: regH = regC; break;
+            case 0x62: regH = regD; break;
+            case 0x63: regH = regE; break;
+
+            case 0x64: break;
+
+            case 0x65: regH = regL; break;
+            case 0x66: regH = _mmu.ReadByte(HL); break;
+            case 0x67: regH = regA; break;
+
+            case 0x68: regL = regB; break;
+            case 0x69: regL = regC; break;
+            case 0x6A: regL = regD; break;
+            case 0x6B: regL = regE; break;
+            case 0x6C: regL = regH; break;
+
+            case 0x6D: break;
+
+            case 0x6E: regL = _mmu.ReadByte(HL); break;
+            case 0x6F: regL = regA; break;
+
+            case 0x70: _mmu.WriteByte(HL, regB); break;
+            case 0x71: _mmu.WriteByte(HL, regC); break;
+            case 0x72: _mmu.WriteByte(HL, regD); break;
+            case 0x73: _mmu.WriteByte(HL, regE); break;
+            case 0x74: _mmu.WriteByte(HL, regH); break;
+            case 0x75: _mmu.WriteByte(HL, regL); break;
+            case 0x76: Halt(); break;
+            case 0x77: _mmu.WriteByte(HL, regA); break;
+
+            case 0x78: regA = regB; break;
+            case 0x79: regA = regC; break;
+            case 0x7A: regA = regD; break;
+            case 0x7B: regA = regE; break;
+            case 0x7C: regA = regH; break;
+            case 0x7D: regA = regL; break;
+            case 0x7E: regA = _mmu.ReadByte(HL); break;
+
+            case 0x7F: break;
+
+            case 0x80: Add(regB); break;
+            case 0x81: Add(regC); break;
+            case 0x82: Add(regD); break;
+            case 0x83: Add(regE); break;
+            case 0x84: Add(regH); break;
+            case 0x85: Add(regL); break;
+            case 0x86: Add(_mmu.ReadByte(HL)); break;
+            case 0x87: Add(regA); break;
+
+            case 0x88: AddCarry(regB); break;
+            case 0x89: AddCarry(regC); break;
+            case 0x8A: AddCarry(regD); break;
+            case 0x8B: AddCarry(regE); break;
+            case 0x8C: AddCarry(regH); break;
+            case 0x8D: AddCarry(regL); break;
+            case 0x8E: AddCarry(_mmu.ReadByte(HL)); break;
+            case 0x8F: AddCarry(regA); break;
+
+            case 0x90: Subtract(regB); break;
+            case 0x91: Subtract(regC); break;
+            case 0x92: Subtract(regD); break;
+            case 0x93: Subtract(regE); break;
+            case 0x94: Subtract(regH); break;
+            case 0x95: Subtract(regL); break;
+            case 0x96: Subtract(_mmu.ReadByte(HL)); break;
+            case 0x97: Subtract(regA); break;
+
+            case 0x98: SubtractCarry(regB); break;
+            case 0x99: SubtractCarry(regC); break;
+            case 0x9A: SubtractCarry(regD); break;
+            case 0x9B: SubtractCarry(regE); break;
+            case 0x9C: SubtractCarry(regH); break;
+            case 0x9D: SubtractCarry(regL); break;
+            case 0x9E: SubtractCarry(_mmu.ReadByte(HL)); break;
+            case 0x9F: SubtractCarry(regA); break;
+
+            case 0xA0: And(regB); break;
+            case 0xA1: And(regC); break;
+            case 0xA2: And(regD); break;
+            case 0xA3: And(regE); break;
+            case 0xA4: And(regH); break;
+            case 0xA5: And(regL); break;
+            case 0xA6: And(_mmu.ReadByte(HL)); break;
+            case 0xA7: And(regA); break;
+
+            case 0xA8: Xor(regB); break;
+            case 0xA9: Xor(regC); break;
+            case 0xAA: Xor(regD); break;
+            case 0xAB: Xor(regE); break;
+            case 0xAC: Xor(regH); break;
+            case 0xAD: Xor(regL); break;
+            case 0xAE: Xor(_mmu.ReadByte(HL)); break;
+            case 0xAF: Xor(regA); break;
+
+            case 0xB0: Or(regB); break;
+            case 0xB1: Or(regC); break;
+            case 0xB2: Or(regD); break;
+            case 0xB3: Or(regE); break;
+            case 0xB4: Or(regH); break;
+            case 0xB5: Or(regL); break;
+            case 0xB6: Or(_mmu.ReadByte(HL)); break;
+            case 0xB7: Or(regA); break;
+
+            case 0xB8: Compare(regB); break;
+            case 0xB9: Compare(regC); break;
+            case 0xBA: Compare(regD); break;
+            case 0xBB: Compare(regE); break;
+            case 0xBC: Compare(regH); break;
+            case 0xBD: Compare(regL); break;
+            case 0xBE: Compare(_mmu.ReadByte(HL)); break;
+            case 0xBF: Compare(regA); break;
+
+            case 0xC0: Return(!FlagZ); break;
+            case 0xC1: BC = Pop(); break;
+            case 0xC2: Return(!FlagZ); break;
+            case 0xC3: Return(true); break;
+            case 0xC4: Call(!FlagZ); break;
+            case 0xC5: Push(BC); break;
+            case 0xC6: Add(_mmu.ReadByte(programCounter++)); break;
+            case 0xC7: Restart(0x00); break;
+
+            case 0xC8: Return(FlagZ); break;
+            case 0xC9: Return(true); break;
+            case 0xCA: Jump(FlagZ); break;
+            case 0xCB: PrefixCB(_mmu.ReadByte(programCounter++)); break;
+            case 0xCC: Call(FlagZ); break;
+            case 0xCD: Call(true); break;
+            case 0xCE: AddCarry(_mmu.ReadByte(programCounter++)); break;
+            case 0xCF: Restart(0x08); break;
+
+            case 0xD0: Return(!FlagC); break;
+            case 0xD1: DE = Pop(); break;
+            case 0xD2: Jump(!FlagC); break;
+            case 0xD4: Call(!FlagC); break;
+            case 0xD5: Push(DE); break;
+            case 0xD6: Subtract(_mmu.ReadByte(programCounter++)); break;
+            case 0xD7: Restart(0x10); break;
+
+            case 0xD8: Return(FlagC); break;
+            case 0xD9: Return(true); ime = true; break;
+            case 0xDA: Jump(FlagC); break;
+            case 0xDC: Call(FlagC); break;
+            case 0xDE: SubtractCarry(_mmu.ReadByte(programCounter++)); break;
+            case 0xDF: Restart(0x18); break;
+
+            case 0xE0: _mmu.WriteByte((ushort)(0xFF00 + _mmu.ReadByte(programCounter++)), regA); break;
+            case 0xE1: HL = Pop(); break;
+            case 0xE2: _mmu.WriteByte((ushort)(0xFF00 + regC), regA); break;
+            case 0xE5: Push(HL); break;
+            case 0xE6: And(_mmu.ReadByte(programCounter++)); break;
+            case 0xE7: Restart(0x20); break;
+
+            case 0xE8: stackPointer = DoubleAddHL(stackPointer); break;
+            case 0xE9: programCounter = HL; break;
+            case 0xEA: _mmu.WriteWord(_mmu.ReadWord(programCounter += 2), regA); break;
+            case 0xEE: Xor(_mmu.ReadByte(programCounter++)); break;
+            case 0xEF: Restart(0x28); break;
+
+            case 0xF0: regA = _mmu.ReadByte((ushort)(0xFF00 + _mmu.ReadByte(programCounter++))); break;
+            case 0xF1: AF = Pop(); break;
+            case 0xF2: regA = _mmu.ReadByte((ushort)(0xFF00 + regC)); break;
+            case 0xF3: ime = false; break;
+            case 0xF5: Push(AF); break;
+            case 0xF6: Or(_mmu.ReadByte(programCounter++)); break;
+            case 0xF7: Restart(0x30); break;
+
+            case 0xF8: HL = DoubleAddHL(stackPointer); break;
+            case 0xF9: stackPointer = HL; break;
+            case 0xFA: regA = _mmu.ReadByte(_mmu.ReadWord(programCounter += 2)); break;
+            case 0xFB: imeEnabler = true; break;
+            case 0xFE: Compare(_mmu.ReadByte(programCounter++)); break;
+            case 0xFF: Restart(0x38); break;
+
+            default: UnsupportedOpCode(opCode); break;
         }
+        cycles += _cyclesValues[opCode];
+        return cycles;
     }
 
     private void PrefixCB(in byte opCode)
@@ -336,13 +525,13 @@ public sealed class Processor
         FlagH = true;
     }
 
-    private void ShiftRightLog(ref byte bit)
+    private void ShiftRightLog(in byte bit)
     {
-        bit = (byte)(bit >> 1);
-        SetFlagZ(bit);
+        var result = (byte)(bit >> 1);
+        SetFlagZ(result);
         FlagN = false;
         FlagH = false;
-        FlagC = (bit & 0x01) != 0;
+        FlagC = (result & 0x01) != 0;
     }
 
     private void Swap(ref byte bit)
@@ -408,14 +597,14 @@ public sealed class Processor
         FlagC = (bit & 0x80) != 0;
     }
 
-    private void DoubleAddHL(ref ushort registry)
+    private ushort DoubleAddHL(in ushort registry)
     {
         var value = _mmu.ReadByte(programCounter++);
         FlagZ = false;
         FlagN = false;
         SetFlagH((byte)registry, value);
         SetFlagC((byte)registry + value);
-        registry = (ushort)(registry + (sbyte)value);
+        return (ushort)(registry + (sbyte)value);
     }
 
     private void JumpRelative(in bool flag)
